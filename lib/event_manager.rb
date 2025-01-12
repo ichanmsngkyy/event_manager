6aefecdf0fa2bf79c6128d261c104986ff957d2d
ichanmsngkyy <ichanmsngky@gmail.com>
2025-01-12 23:45:28 +0800

    Added html and fix extracting legislator names

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,28 +5,39 @@ require 'google/apis/civicinfo_v2'
 
 puts 'Event Manager Initialized'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = File.read('secret.key').strip
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = File.read('secret.key').strip
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+    legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name)
+
+    legislator_names.join(', ')
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 end
 
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 contents.each do |row|
   name = row [:first_name]
+
   zipcode = clean_zipcode(row[:zipcode])
 
-  begin
-    legislators = civic_info.representative_info_by_address(
-      address: zipcode,
-      levels: 'country',
-      roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-    legislators = legislators.officials
-  rescue StandardError
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-  end
+  legislators = legislators_by_zipcode(zipcode)
 
   puts "#{name} #{zipcode} #{legislators}"
+
+  template_letter = File.read('form_letter.html')
 end
