c5ab14486b959fcc3e86bbb834fd5d6b6a887086
ichanmsngkyy <ichanmsngky@gmail.com>
2025-01-13 16:37:02 +0800

    Finished implementing functionalities
    Added a function to clean phone numbers
    Included peak hours and dates during registration

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,6 +2,8 @@
 
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
+require 'date'
 
 puts 'Event Manager Initialized'
 
@@ -19,29 +21,75 @@ def legislators_by_zipcode(zip)
       levels: 'country',
       roles: %w[legislatorUpperBody legislatorLowerBody]
     )
-    legislators = legislators.officials
-
-    legislator_names = legislators.map(&:name)
-
-    legislator_names.join(', ')
   rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+def clean_number(number)
+  digits = number.gsub(/\D/, '')
+  if digits.length == 10
+    digits
+  elsif digits.length == 11 && digits[0] == '1'
+    digits [1..]
+  else
+    'Bad number'
+  end
+end
+
+def hour(time)
+  DateTime.strptime(time, '%m/%d/%y %H:%M').hour
+end
+
+def extract_day(day)
+  DateTime.strptime(day, '%m/%d/%y %H:%M').wday
+end
+
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
-template_letter = File.read('form_letter.html')
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+hourly_count = Hash.new(0)
+day_count = Hash.new(0)
+
+day_names = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday Sunday]
+
 contents.each do |row|
+  id = row[0]
   name = row [:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
+  number = clean_number(row[:homephone])
+  time = hour(row[:regdate])
+  day = extract_day(row[:regdate])
 
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
+  hourly_count[time] += 1
+  day_count[day] += 1
 
-  puts personal_letter
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
+  puts "#{name} (Phone: #{number}, Registered at Hour: #{time}, Day: #{day_names[day]})"
+end
+
+puts 'Peak Registration Hours'
+hourly_count.sort_by {|_hour,count| -count}.each do |hour,count|
+  puts "Hour #{hour}: #{count} registrations"
+end
 
 
+puts 'Peak Registration Day'
+day_count.sort_by {|_day,count| -count}.each do |day, count|
+  puts "Day #{day_names[day]}: #{count} registrations"
 end
